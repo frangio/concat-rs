e696c3719796d02d5a762c9db3d8031cc9f04775
Francisco Giordano <frangio.1@gmail.com>
2015-05-13 02:01:41 -0300

    clean up documentation a bit

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-//! Provides an adapter to read sequentially from the items of an
-//! iterator.
+//! Provides the Concat reader-iterator adaptor, a reader each of whose instances is built from an
+//! iterator of readers and takes its contents from the iterator's items sequentially. Thus, the
+//! contents read from a Concat instance will be the concatenation of the items' contents, as if by
+//! repeatedly chaining them.
 
 use std::io::{Read, Result};
 
-/// Creates an instance of a reader that reads sequentially from the
-/// items of an iterator.
 pub fn cat<R, I>(mut iter: I) -> Cat<R, I>
 where R: Read, I: Iterator<Item=R> {
     let curr = iter.next();
@@ -15,7 +15,6 @@ where R: Read, I: Iterator<Item=R> {
     }
 }
 
-/// A reader that reads sequentially from the items in an iterator.
 pub struct Cat<R, I>
 where R: Read, I: Iterator<Item=R> {
     iter: I,
@@ -24,12 +23,10 @@ where R: Read, I: Iterator<Item=R> {
 
 impl<R, I> Cat<R, I>
 where R: Read, I: Iterator<Item=R> {
-    /// Returns a reference to the item last read, or None if the
-    /// iterator has been exhausted.
+    /// Returns a reference to the item last read, or None if the iterator has been exhausted.
     ///
-    /// This is useful for error handling and reporting: if a read
-    /// operation fails, the reference returned will point to the item
-    /// which caused the the error.
+    /// This is useful for error handling and reporting: if a read operation fails, the reference
+    /// returned will point to the item which caused the the error.
     pub fn current(&self) -> Option<&R> {
         self.curr.as_ref()
     }
@@ -46,8 +43,7 @@ where R: Read, I: Iterator<Item=R> {
         if n > 0 || buf.len() == 0 || self.curr.is_none() {
             Ok(n)
         } else {
-            // The current reader reached the end so we have to advance
-            // the iterator and try again.
+            // The current reader reached the end so we have to advance the iterator and try again.
             self.curr = self.iter.next();
             self.read(buf)
         }
